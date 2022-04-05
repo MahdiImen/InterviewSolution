af80a45b5f73808dc523978d54fb587ab47a8d14
MahdiImen <imen.mahdi@supcom.tn>
2022-04-05 23:55:38 +0100

    Exercise3 Solution

--- Exercise 3/exercise.py
+++ Exercise 3/exercise.py
@@ -1,9 +1,22 @@
 #!/usr/bin/env python3
 
+def initial_lanternfish_list(filename):
+    with open(filename) as f:                           #Open the file and read the line
+        lanternfishs = f.readline().strip().split(',')  
+    return list(map(int, lanternfishs))                 #Return a list with the initial lanternfish list
 
 def lanternfish(filename):
-    # TODO
-    return 0
+    lanternfishs = initial_lanternfish_list(filename)   #Initilize the lanternfish ages and number
+    for day in range(80):                               #Iterate over 80 days
+        new_fish = 0                                    #Number of fish to be born each day
+        for i, fish in enumerate(lanternfishs):
+            if fish == 0:                               #If a fish reaches the end of its cycle
+                lanternfishs[i] = 6                     #It restarts the cycle at 6
+                new_fish += 1                           #And a new fish is born
+            else:
+                lanternfishs[i] -= 1
+        lanternfishs += [8]*new_fish                    #At the end of a day, we add newly born fish to the list
+    return len(lanternfishs)                            #Return the total number of lanterfish
 
 
 def main():
