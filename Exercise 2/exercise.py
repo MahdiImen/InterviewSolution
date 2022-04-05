f691edf151870d9a44644075e53a193c99652187
MahdiImen <imen.mahdi@supcom.tn>
2022-04-05 21:31:15 +0100

    Exercise2 Solution

--- Exercise 2/exercise.py
+++ Exercise 2/exercise.py
@@ -1,9 +1,26 @@
 #!/usr/bin/env python3
 
+def generator(filename):
+    with open(filename) as f:   #Open the file and read each line
+        for line in f:
+            yield int(line.strip('\n')) #retrieve each depth measurement one at a time
 
 def sonar(filename):
-    # TODO
-    return 0
+    n=0                                     #Initialize counter to 0
+    gen = generator(filename)               #Create a generator to retrieve the depth measurements 
+    try:        
+        window_A = []                       #Create first window
+        for i in range(3):                  #Get first three measurements
+            window_A.append(next(gen))                           
+        while(True):
+            window_B = window_A[1:]         #Get the first and second measurements of the next window
+            window_B.append(next(gen))      #Get the third measurement next window
+            if sum(window_B)>sum(window_A) :#If the sum of the second window is bigger
+                n += 1                      #We increase the counter
+            window_A = window_B[:]      
+    except StopIteration:                   #When the we reach the end of file, the generator raises a StopIteration. 
+        return n                            #We then return the number obtained
+
 
 
 def main():
