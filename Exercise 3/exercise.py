05a4fcf181d50dc47ae096e01d2e7be888cc09c2
MahdiImen <imen.mahdi@supcom.tn>
2022-04-06 01:32:31 +0100

    Optimized Solution for Exercise3

--- Exercise 3/exercise.py
+++ Exercise 3/exercise.py
@@ -5,19 +5,36 @@ def initial_lanternfish_list(filename):
         lanternfishs = f.readline().strip().split(',')  
     return list(map(int, lanternfishs))                 #Return a list with the initial lanternfish list
 
-def lanternfish(filename):
-    lanternfishs = initial_lanternfish_list(filename)   #Initilize the lanternfish ages and number
-    for day in range(80):                               #Iterate over 80 days
-        new_fish = 0                                    #Number of fish to be born each day
-        for i, fish in enumerate(lanternfishs):
-            if fish == 0:                               #If a fish reaches the end of its cycle
-                lanternfishs[i] = 6                     #It restarts the cycle at 6
-                new_fish += 1                           #And a new fish is born
-            else:
-                lanternfishs[i] -= 1
-        lanternfishs += [8]*new_fish                    #At the end of a day, we add newly born fish to the list
-    return len(lanternfishs)                            #Return the total number of lanterfish
+#The following recursive function will calculate the 
+#number of grandchildren of one fish of age 6 after
+# a given number of days 
+def grandchildren(days_left): 
+  if days_left < 7:
+    return 0
+  fish_born = 0
+  for i in range(days_left//7):
+    fish_born += grandchildren(days_left-2-(i+1)*7)
+  return fish_born + (days_left // 7)
 
+
+def lanternfish(filename):
+    #Initilize the lanternfish ages and number
+    lanternfishs = initial_lanternfish_list(filename) 
+    #There are 6 ages possibles for the initial state : [0,1,2,3,4,5,6]  
+    #For each age, we will determine how much one fish at that age can
+    #have as grandchildren after 80 days
+    #PS1: The number of grandchildren of a fish of age n on day d
+    #is the same on the day d+(6-n)
+    #PS2: The function grandchildren assumes that the fish is of age 6
+    grandchildren_born = list(map(grandchildren, [86,85,84,83,82,81,80] )) 
+    #The initial number of lanternfish
+    n = len(lanternfishs)
+    #For each lanternfish in the initial list we add the total number of 
+    #its grandchildren after 80 days 
+    for fish in lanternfishs:
+        n += grandchildren_born[fish]
+    #Return the total number of lanterfish
+    return n                           
 
 def main():
     with open("output.txt") as f:
